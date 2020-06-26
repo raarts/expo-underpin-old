86808324b8aaf9d4346f87d03fa5154ecbf99825
Ron Arts <ron.arts@gmail.com>
2020-06-26 23:43:47 +0200

    yarn add redux-logger; yarn add @types/redux-logger -D  Add redux-logger.

--- store/index.ts
+++ store/index.ts
@@ -1,6 +1,7 @@
 import { configureStore, combineReducers, getDefaultMiddleware } from '@reduxjs/toolkit';
 import Constants from 'expo-constants';
 import { persistStore, persistReducer } from 'redux-persist';
+import { createLogger } from 'redux-logger';
 import AsyncStorage from '@react-native-community/async-storage';
 import { FLUSH, PAUSE, PERSIST, PURGE, REGISTER, REHYDRATE } from 'redux-persist/es/constants';
 import systemReducer from './system';
@@ -8,6 +9,7 @@ import systemReducer from './system';
 const rootReducer = combineReducers({
   darkMode: systemReducer,
 });
+export type RootState = ReturnType<typeof rootReducer>;
 
 const persistConfig = {
   key: Constants.manifest.slug || 'root',
@@ -15,16 +17,31 @@ const persistConfig = {
 };
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const store = configureStore({
-  reducer: persistedReducer,
-  middleware: getDefaultMiddleware({
+// build the list of middleware to apply to store actions
+const middleware = [
+  ...getDefaultMiddleware({
     serializableCheck: {
       ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
     },
   }),
+];
+
+// redux-logger. Only activate when needed. This should be the last entry in the middleware array
+const { pathname } = window.location || {};
+const IS_RUNNING_IN_CHROME = pathname && pathname.indexOf('debugger-ui');
+const IS_PRODUCTION = process.env.NODE_ENV === 'production';
+const logger = createLogger({
+  // only log if the predicate function returns true
+  predicate: (getState, action) => action.type !== '',
+});
+if (IS_RUNNING_IN_CHROME && !IS_PRODUCTION) {
+  middleware.push(logger);
+}
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware,
 });
 export const persistor = persistStore(store);
-
-export type RootState = ReturnType<typeof rootReducer>;
 
 export default store;
