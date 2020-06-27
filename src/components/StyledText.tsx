884c00a6dfb14783607ba23140832e4fdc869558
Ron Arts <ron.arts@gmail.com>
2020-06-28 00:07:48 +0200

    Use ThemeProvider instead of StyleSheet for the MonoText component.

--- src/components/StyledText.tsx
+++ src/components/StyledText.tsx
@@ -1,7 +1,16 @@
 import * as React from 'react';
 import { Text, TextProps } from './Themed';
+import ThemeProvider, { applyTheme } from '../underpin/ThemeProvider';
 
 export function MonoText(props: TextProps): React.ReactElement {
   const { style } = props;
-  return <Text {...props} style={[style, { fontFamily: 'space-mono' }]} />;
+  const styles = applyTheme(localStyles);
+  return <Text {...props} style={[style, styles.text]} />;
 }
+
+const styles = ThemeProvider.create({
+  text: {
+    fontFamily: 'space-mono',
+  },
+});
+const localStyles = styles;
