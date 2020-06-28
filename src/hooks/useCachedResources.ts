330461ec515a9db7be97d81345306e756a4cb526
Ron Arts <ron.arts@gmail.com>
2020-06-28 09:11:43 +0200

    Load assets and Fonts async at startup and show the splash screen while doing it.

--- src/hooks/useCachedResources.ts
+++ src/hooks/useCachedResources.ts
@@ -1,7 +1,18 @@
 import { Ionicons } from '@expo/vector-icons';
+import { Image } from 'react-native';
 import * as Font from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
 import * as React from 'react';
+import { Asset } from 'expo-asset';
+
+const imageList: number[] | string[] = [require('../../assets/images/splash.png')];
+
+const fontList: (string | { [fontFamily: string]: Font.FontSource })[] = [
+  {
+    ...Ionicons.font,
+    'space-mono': require('../../assets/fonts/SpaceMono-Regular.ttf'),
+  },
+];
 
 export default function useCachedResources(): boolean {
   const [isLoadingComplete, setLoadingComplete] = React.useState(false);
@@ -12,11 +23,33 @@ export default function useCachedResources(): boolean {
       try {
         await SplashScreen.preventAutoHideAsync();
 
-        // Load fonts
-        await Font.loadAsync({
-          ...Ionicons.font,
-          'space-mono': require('../../assets/fonts/SpaceMono-Regular.ttf'),
-        });
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        const appPromises: any[] = [
+          // This is the place to put non-Underpin async functions
+        ];
+
+        // get and cache images
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        const cacheImages = (images: unknown[]): any[] => {
+          return images.map((image) => {
+            if (typeof image === 'string') {
+              return Image.prefetch(image);
+            }
+            return Asset.fromModule(image as string | number).downloadAsync();
+          });
+        };
+        const imageAssets = cacheImages(imageList);
+
+        // get and cache fonts
+        const cacheFonts = (fonts: (string | { [fontFamily: string]: Font.FontSource })[]): Promise<void>[] => {
+          return fonts.map(
+            (font: string | { [fontFamily: string]: Font.FontSource }): Promise<void> => Font.loadAsync(font),
+          );
+        };
+        const fontAssets = cacheFonts(fontList);
+
+        // wait for all promises to resolve
+        await Promise.all(appPromises.concat([...imageAssets, ...fontAssets]));
       } catch (e) {
         // We might want to provide this error information to an error reporting service
         // eslint-disable-next-line no-console
